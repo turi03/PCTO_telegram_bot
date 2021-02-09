cc45a5f9d6ab26332a8703ed0bd199d2fbd5d45f
turi03 <59514421+turi03@users.noreply.github.com>
2021-02-09 18:10:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,15 +14,14 @@ bot = telebot.TeleBot(API_TOKEN)
 # Handle '/start' and '/help'
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-    bot.reply_to(message, """\
-ciao sono un primo bot creato per prova\
-""")
+    bot.reply_to(message, """Ciao, questo è il progetto PCTO, classe 4BT-i, A.S 2020/2021""")
+    bot.reply_to(message, """progetto di Coman E., Franturi M. e Vignoli M.""")
 
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
 
 def echo_message(message):
-  if message.text=='ciao':
-    bot.reply_to(message,'ciao anche a te')
+  if message.text=='Ciao':
+    bot.reply_to(message,'ciao ancha te')
 bot.polling()
