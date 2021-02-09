ae02a87ae6740a02413fdfde70eaf0e678d90c37
turi03 <59514421+turi03@users.noreply.github.com>
2021-02-09 15:45:21 +0100

    Update bot_main.py

--- bot_main.py
+++ bot_main.py
@@ -32,3 +32,24 @@ def main():
 
 if __name__=='__main__':
    main()
+
+
+
+
+
+$ python bot_main.py
+$ pip install python-telegram-bot --upgrade
+
+// Crea il tuo bot passando il token ricevuto da @BotFather 
+//TelegramBot bot =  new  TelegramBot ( "1459713391:AAHymmVVZxIxVYpoExjJFTR7tx9kSQolYCI" );
+
+// Registrati per il 
+bot degli aggiornamenti . setUpdatesListener (aggiornamenti - > {
+     // ... aggiornamenti di processo 
+    // ritorno id del aggiornamento o confermarle tutte elaborati 
+    ritorno  UpdatesListener . CONFIRMED_UPDATES_ALL ;
+});
+
+// Invia messaggi 
+lunghi chatId = aggiornamento . messaggio () . chat () . id ();
+SendResponse response = bot . execute ( new  SendMessage (chatId, " Hello! " ));
